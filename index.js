712768dfc2ed61b631cc94f218ac35fe50889cf3
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-05 17:32:37 +0300

    Added the post method, able to fetch the data from rendered server, displayed the fetched data in form of card and styled the cards and the page

---
+++ index.js
@@ -0,0 +1,137 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector(".add-recipe")
+    .addEventListener("submit", handleSubmit);
+    
+    function initialize() {
+        getRecipes();
+      }
+      initialize();
+  // Event handler for the event listener
+  function handleSubmit(event) {
+    event.preventDefault();
+    let RecipeId = Math.floor(Math.random() * 1000);
+    let recipeObject = {
+      id: RecipeId,
+      name: event.target.name.value,
+      ingredients: event.target.ingredients.value,
+      instructions: event.target.instructions.value,
+      time_taken: event.target.time_taken.value,
+    };
+    renderRecipes(recipeObject);
+    addRecipe(recipeObject);
+    formVisibility();
+  }
+
+  // Fetch request for recipes as well as Patch, Post, and Delete
+  function getRecipes() {
+    fetch("https://json-jzhe.onrender.com/Recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((recipe) => {
+          renderRecipes(recipe);
+        });
+      });
+  }
+
+  function addRecipe(recipeObject) {
+    fetch("https://json-jzhe.onrender.com/Recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipeObject),
+    })
+      .then((response) => response.json())
+      .then((recipe) => console.log(recipe));
+  }
+
+  // DOM manipulation functions
+  //renders the recipes it has fetched.
+  function renderRecipes(recipe) {
+    const recipeDeets = document.querySelector(".meal-details");
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const name = document.createElement("h2");
+    name.innerHTML = recipe.name;
+    card.appendChild(name);
+
+    const ViewRecipebtn = document.createElement("button");
+    ViewRecipebtn.innerHTML = "View Recipe";
+    ViewRecipebtn.addEventListener("click", () => {
+      showRecipeDetails(recipe, card);
+    });
+
+    card.appendChild(ViewRecipebtn);
+    recipeDeets.appendChild(card);
+  }
+
+  //this functions shows the card with the rendered details
+  function showRecipeDetails(recipe, card) {
+    const modal = document.createElement("div");
+    modal.className = "modal";
+
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+
+    const closeBtn = document.createElement("span");
+    closeBtn.className = "close";
+    closeBtn.innerHTML = "&times;";
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+      removeBlur();
+    });
+    modalContent.appendChild(closeBtn);
+
+    const recipeCard = document.createElement("div");
+    recipeCard.innerHTML = `
+        <h2 id="recipe-name">${recipe.name}</h2>
+        <h3> Ingredients:</h3>
+        <p id="recipe-ingredients">${recipe.ingredients}</p>
+        <h3>Instructions:</h3>
+        <p id="recipe-instructions">${recipe.instructions} minutes</p>
+        <p id="recipe-time"><strong>Time Needed:</strong> ${recipe.time_taken}</p>
+      `;
+
+    modalContent.appendChild(recipeCard);
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+    modal.style.display = "block";
+    applyBlur(card);
+
+    //this function removes the blur effect when a card is closed
+    function removeBlur() {
+      document.querySelectorAll(".card").forEach((cardElement) => {
+        cardElement.classList.remove("blur-effect");
+      });
+      document.querySelector(".add-recipe").classList.remove("blur-effect");
+    }
+  }
+
+  //this function applies the blur effect on the rest of the page when a card is opened
+  function applyBlur(card) {
+    document.querySelectorAll(".card").forEach((cardElement) => {
+      if (cardElement !== card) {
+        cardElement.classList.add("blur-effect");
+      }
+    });
+    document.querySelector(".add-recipe").classList.add("blur-effect");
+  }
+
+  const toggleButton = document.querySelector(".form-opener");
+  const myForm = document.querySelector(".add-recipe");
+
+  toggleButton.addEventListener("click", formVisibility);
+
+  // Function to toggle the visibility of the form
+  function formVisibility() {
+    if (myForm.style.display === "none") {
+      myForm.style.display = "block";
+    } else {
+      myForm.style.display = "none";
+    }
+  }
+});
+
